939a6d05dd614e81d9759f7d48c9db77dbe0f426
kousik nandy <kousiknandy@gmail.com>
2026-08-28 14:23:32 +0000

    image processing path utility
    Signed-off-by: kousik nandy <kousiknandy@gmail.com>

---
+++ image_processing/utils/path_utils.py
@@ -0,0 +1,48 @@
+from pathlib import PurePath
+
+OUTPUT = "output"
+
+
+def get_output_path(image_path: str, transform_path: str):
+    ip = PurePath(image_path)
+    tp = PurePath(transform_path)
+    op = OUTPUT / PurePath(*list(ip.parent.parts)[1:])
+    fn = "-".join([ip.stem] + list(tp.parts)[1:-1] + [tp.stem]) + ip.suffix
+    return str(op / fn)
+
+
+if __name__ == "__main__":
+    assert (
+        get_output_path("images/x11/zx.png", "transforms/flip.json")
+        == OUTPUT + "/x11/zx-flip.png"
+    ), get_output_path("images/x11/zx.png", "transforms/flip.json")
+
+    assert (
+        get_output_path("images/x11/x12/x13/zx.png", "transforms/flip.json")
+        == OUTPUT + "/x11/x12/x13/zx-flip.png"
+    ), get_output_path("images/x11/x12/x13/zx.png", "transforms/flip.json")
+
+    assert (
+        get_output_path("images/x11/zx.png", "transforms/x12/flip.json")
+        == OUTPUT + "/x11/zx-x12-flip.png"
+    ), get_output_path("images/x11/zx.png", "transforms/x12/flip.json")
+
+    assert (
+        get_output_path("images/x11/x12/zx.png", "transforms/x13/flip.json")
+        == OUTPUT + "/x11/x12/zx-x13-flip.png"
+    ), get_output_path("images/x11/x12/zx.png", "transforms/x13/flip.json")
+
+    assert (
+        get_output_path("images/zx.png", "transforms/flip.json")
+        == OUTPUT + "/zx-flip.png"
+    ), get_output_path("images/x11/x12/zx.png", "transforms/x13/flip.json")
+
+    assert (
+        get_output_path("images/x11/zx.jpg", "transforms/flip.json")
+        == OUTPUT + "/x11/zx-flip.jpg"
+    ), get_output_path("images/x11/zx.jpg", "transforms/flip.json")
+
+    assert (
+        get_output_path("images/x11/zx.png", "transforms/flip.yaml")
+        == OUTPUT + "/x11/zx-flip.png"
+    ), get_output_path("images/x11/zx.png", "transforms/flip.yaml")
