efa4db748faacd33a61da44dde8259d6a603c8e0
kousik nandy <kousiknandy@gmail.com>
2026-08-30 13:14:50 +0000

    image processing: process loop
    Signed-off-by: kousik nandy <kousiknandy@gmail.com>

---
+++ image_processing/pipeline_processpool.py
@@ -0,0 +1,34 @@
+from typing import Callable
+
+from utils.path_utils import get_output_path, walk_files
+from itertools import product
+
+from concurrent.futures import ProcessPoolExecutor
+
+from image_processor import transform_image
+
+image_files = {"*.jpg", "*.jpeg", "*.png"}
+xform_files = {"*.json"}
+
+
+def transform_image_t(p):
+    transform_image(*p)
+
+
+def process_images(
+    image_dir: str,
+    transformation_dir: str,
+    get_output_path: Callable[[str, str], str],
+) -> None:
+    images = walk_files(image_dir, image_files)
+    xforms = walk_files(transformation_dir, xform_files)
+    ops = (
+        (i, t, "/var/tmp/" + get_output_path(i, t)) for i, t in product(images, xforms)
+    )
+    with ProcessPoolExecutor(max_workers=8) as executor:
+        for res in executor.map(transform_image_t, ops):
+            pass
+
+
+if __name__ == "__main__":
+    process_images("images", "transforms", get_output_path)
