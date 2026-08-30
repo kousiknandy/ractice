ef0867cc246a0d17415bbce96d3ef7d477648bec
kousik nandy <kousiknandy@gmail.com>
2026-08-30 12:13:50 +0000

    image processing: threaded loop
    Signed-off-by: kousik nandy <kousiknandy@gmail.com>

---
+++ image_processing/pipeline_threadpool.py
@@ -0,0 +1,32 @@
+from typing import Callable
+
+from utils.path_utils import get_output_path, walk_files
+from itertools import product
+
+from concurrent.futures import ThreadPoolExecutor
+
+from image_processor import transform_image
+
+image_files = {"*.jpg", "*.jpeg", "*.png"}
+xform_files = {"*.json"}
+
+
+def process_images(
+    image_dir: str,
+    transformation_dir: str,
+    get_output_path: Callable[[str, str], str],
+) -> None:
+    images = walk_files(image_dir, image_files)
+    xforms = walk_files(transformation_dir, xform_files)
+    ops = (
+        (str(i), str(t), "/var/tmp/" + get_output_path(i, t))
+        for i, t in product(images, xforms)
+    )
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        res = executor.map(lambda p: transform_image(*p), ops)
+    for r in res:
+        pass
+
+
+if __name__ == "__main__":
+    process_images("images", "transforms", get_output_path)
